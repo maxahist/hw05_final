80a908315bcf8ef71cc1a2f497e54874b7ff3779
maxahist <tsihaxam@gmail.com>
2022-09-19 00:23:47 +0500

    12_23

--- yatube/core/constants/constants.py
+++ yatube/core/constants/constants.py
@@ -1,3 +1,13 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
+
+def get_object_or_none(klass, *args, **kwargs):
+    try:
+        return get_object_or_404(klass, *args, **kwargs)
+    except Http404:
+        return None
+
 ELEMENTS_PER_PAGE = 10
 
 CACHE_SECONDS = 20
